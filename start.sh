de83f974d53a86e4c6a586f5cb1bc28fd3d715cf
Daniil Podovinnikov <pdaniil136@gmail.com>
2025-12-09 13:09:30 +0300

    add nessie

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-docker compose -p lakehouse -f docker-compose-postgres.yml \
-  -f docker-compose-bootstrap-db.yml \
-  -f docker-compose.yml up -d
+docker compose -p lakehouse -f docker-compose.yml up -d
 
-
-source polaris/create_catalog.sh
